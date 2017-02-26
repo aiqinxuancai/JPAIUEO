2e8377eadd0a6a240e3f79bb12b3c2144b45ec04
爱琴炫彩 <aiqinxuancai@163.com>
2017-02-26 17:56:20 +0800

    首次提交

---
+++ JPAIUEO/Base/YinData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JPAIUEO.Base
+{
+    class Yin
+    {
+        public string ping { get; set; }
+        public string pian { get; set; }
+        public string luoma { get; set; }
+    }
+
+    
+
+    class YinData
+    {
+        static ArrayList listYin = new ArrayList();
+
+        public static void InitData()
+        {
+            //listYin.Add(new Yin { ping = "あ",  pian = , luoma = "a"  });
+            var text = Properties.Resources.YinText.Replace("\r", "");
+            var data = text.Split("\n".ToCharArray());
+
+            for ( int i = 0; i < data.Length; i++)
+            {
+                Yin y = new Yin { ping = data[i], pian = data[i + 1], luoma = data[i + 2] };
+
+                listYin.Add(y);
+
+                i = i + 2;
+
+            }
+
+            //var jsonVer = JsonConvert.SerializeObject(listYin);
+            //File.WriteAllText("data.json", jsonVer);
+            
+            //JsonConvert.DeserializeObject<Yin[]>(jsonVer)
+
+            //GetYin("a");
+        }
+
+        public static Yin GetYin(string _luoma)
+        {
+            var yin = (Yin)listYin.ToArray().First(e => ((Yin)e).luoma == _luoma);
+            //var data = from Yin y in listYin where y.luoma == _luoma select y;
+            return yin;
+        }
+
+        public static Yin GetYinRandom()
+        {
+            Random a = new Random();
+            var id = a.Next(0, listYin.Count - 1);
+            return (Yin)listYin[id];
+        }
+
+
+
+    }
+}
---
+++ JPAIUEO/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using JPAIUEO.Base;
+
+namespace JPAIUEO
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AllowsTransparency = true;
+            this.btnMenuMain.ContextMenu = null;
+            var t = new DispatcherTimer(TimeSpan.FromSeconds(5), DispatcherPriority.Normal, Tick, this.Dispatcher);
+            
+        }
+
+        void Tick(object sender, EventArgs e)
+        {
+            var dateTime = DateTime.Now;
+            transitioning.Content = new TextBlock { Text = "Transitioning Content! " + dateTime, SnapsToDevicePixels = true , TextAlignment = TextAlignment.Center};
+        }
+
+
+        private void textBlockMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 主菜单点击打开
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMenuMain_Click(object sender, RoutedEventArgs e)
+        {
+            this.menuMain.PlacementTarget = this.btnMenuMain;
+            this.menuMain.Placement = PlacementMode.Bottom;
+            this.menuMain.IsOpen = true;
+        }
+
+
+        private void textBlockMainLuoMa_MouseEnter(object sender, MouseEventArgs e)
+        {
+            textBlockMainLuoMa.Foreground = new SolidColorBrush(Color.FromRgb(54, 54, 54));
+        }
+
+        private void textBlockMainLuoMa_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBlockMainLuoMa.Foreground = new SolidColorBrush(Color.FromArgb(0, 255, 255, 255));
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuMainClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void textBlockMain_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var data = YinData.GetYinRandom();
+
+            textBlockMain.Text = data.ping;
+            textBlockMainPianJia.Text = data.pian;
+            textBlockMainLuoMa.Text = data.luoma;
+            textBlockMainLuoMa.Foreground = new SolidColorBrush(Color.FromArgb(0, 255, 255, 255));
+        }
+    }
+}
